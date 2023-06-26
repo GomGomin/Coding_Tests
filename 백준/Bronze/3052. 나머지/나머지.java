52d061ad2072f1c9913a67b679494b0dc2e7b4ad
GomGomin <107527861+GomGomin@users.noreply.github.com>
2023-06-26 22:19:59 +0900

    [Bronze II] Title: 나머지, Time: 124 ms, Memory: 14188 KB -BaekjoonHub

---
+++ 백준/Bronze/3052. 나머지/나머지.java
@@ -0,0 +1,24 @@
+
+import java.io.*;
+import java.util.*;
+
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
+        HashSet<Integer> set = new HashSet<Integer>();
+        for(int i =0; i<10; i++) {
+        	int n = Integer.parseInt(br.readLine());
+        	set.add(n % 42);
+        }
+        
+        bw.write(String.valueOf(set.size()));
+        
+        bw.flush();
+        bw.close();
+	}
+	
+}
