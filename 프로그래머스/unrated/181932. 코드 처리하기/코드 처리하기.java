df468795547b0f3c48dbf06badd87ae8f5805962
GomGomin <107527861+GomGomin@users.noreply.github.com>
2023-06-27 10:59:47 +0900

    [unrated] Title: 코드 처리하기, Time: 0.14 ms, Memory: 80.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181932. 코드 처리하기/코드 처리하기.java
@@ -0,0 +1,24 @@
+class Solution {
+    public String solution(String code) {
+        String answer = "";
+        String[] str = code.split("");
+        int mode = 0;
+        for(int i = 0; i<str.length; i++){
+            if(mode == 0){
+                if(str[i].equals("1")){
+                    mode = 1;
+                }else if(i % 2 == 0){
+                    answer += str[i];
+                }
+            }else if(mode == 1){
+                if(str[i].equals("1")){
+                    mode = 0;
+                }else if(i % 2 == 1){
+                    answer += str[i];
+                }
+            }
+        }
+        
+        return answer.length() == 0 ? "EMPTY" : answer;
+    }
+}
