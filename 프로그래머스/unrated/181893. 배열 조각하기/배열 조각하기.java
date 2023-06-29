f87e29b500a1e067a14cca9f3eaccc437c86e5d7
KimNamHun <107527861+GomGomin@users.noreply.github.com>
2023-06-29 14:36:49 +0900

    [unrated] Title: 배열 조각하기, Time: 4.50 ms, Memory: 73.2 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181893. 배열 조각하기/배열 조각하기.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.util.stream.Collectors;
+
+class Solution {
+    public int[] solution(int[] arr, int[] query) {
+        List<Integer> list = Arrays.stream(arr).boxed().collect(Collectors.toList());
+        for(int i = 0; i<query.length; i++){
+            if(i % 2 == 0){
+               list.subList(query[i]+1,list.size()).clear();
+            }else{
+               list.subList(0,query[i]).clear();
+                }
+            }
+
+        return list.stream().mapToInt(i -> i).toArray();
+    }
+}
