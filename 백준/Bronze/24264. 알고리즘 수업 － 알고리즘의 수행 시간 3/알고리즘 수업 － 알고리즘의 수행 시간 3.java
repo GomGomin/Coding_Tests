0a96a5c47b7c9034a0bf7171c3871290f8f72022
KimNamHun <107527861+GomGomin@users.noreply.github.com>
2023-07-10 23:18:48 +0900

    [Bronze III] Title: 알고리즘 수업 - 알고리즘의 수행 시간 3, Time: 124 ms, Memory: 14312 KB -BaekjoonHub

---
+++ 백준/Bronze/24264. 알고리즘 수업 － 알고리즘의 수행 시간 3/알고리즘 수업 － 알고리즘의 수행 시간 3.java
@@ -0,0 +1,24 @@
+
+import java.io.*;
+import java.lang.reflect.Array;
+import java.math.BigInteger;
+import java.util.*;
+
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
+        long n = Long.parseLong(br.readLine());
+        
+        bw.write(String.valueOf(n*n) + "\n" + "2");
+        
+        
+        bw.flush();
+        bw.close();
+	}
+
+}
+ 
