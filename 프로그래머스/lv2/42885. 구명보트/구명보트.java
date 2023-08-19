017a06b3dc3067c73055da285019bc76945c7925
KimNamHun <107527861+GomGomin@users.noreply.github.com>
2023-08-19 18:46:10 +0900

    [level 2] Title: 구명보트, Time: 7.73 ms, Memory: 57 MB -BaekjoonHub

--- 프로그래머스/lv2/42885. 구명보트/구명보트.java
+++ 프로그래머스/lv2/42885. 구명보트/구명보트.java
@@ -1,20 +1,21 @@
-import java.util.Arrays;
+import java.util.*;
 
 class Solution {
     public int solution(int[] people, int limit) {
-        Arrays.sort(people);
         int answer = 0;
-        int left = 0;
-        int right = people.length - 1;
+        Arrays.sort(people);
+        int weigth = 0;
         
-        while (left <= right) {
-            if (people[left] + people[right] <= limit) {
-                left++;
+        for(int i : people){
+            if(weigth + i > limit){
+                answer++;
+                weigth = 0;
             }
-            right--;
+            weigth += i;
+        }
+        if(weigth != 0){
             answer++;
         }
-        
         return answer;
     }
 }
